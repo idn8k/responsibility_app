aa36877f22fff1d0f1c1e181025c3ce29f545f7e
Idan Benik <129896903+idn8k@users.noreply.github.com>
2025-03-04 11:01:22 +0100

    connecting tasks db

--- components/TaskCard.jsx
+++ components/TaskCard.jsx
@@ -3,19 +3,28 @@ import styled from "styled-components";
 
 const StyledTaskContainer = styled.div`
   display: flex;
-  justify-content: space-around;
-  background: lightblue;
+  justify-content: space-between;
+  background: #fff;
+  border: solid 1px #ff3566;
+  box-shadow: 4px 4px 4px 0px rgba(0, 0, 0, 0.1);
+
   width: 80%;
-  padding: 6px 12px;
+  padding: 6px 22px;
   border-radius: 16px;
+`;
+
+const StyledTaskName = styled.p`
+  font-size: 22px;
+  height: 24px;
+  line-height: 24px;
+  text-align: center;
 `;
 
 export default function TaskCard({ task }) {
   const { taskName } = task;
   return (
     <StyledTaskContainer>
-      <p>{taskName}</p>
-      <input type="checkbox" />
+      <StyledTaskName>{taskName}</StyledTaskName>
     </StyledTaskContainer>
   );
 }
