114459262d1c5dcbb98c1d60e84a78853ebe9a34
Idan <idan.benik@gmail.com>
2025-03-07 15:32:09 +0100

    add toggle switch functionality

--- pages/_app.js
+++ pages/_app.js
@@ -8,11 +8,18 @@ import { useState } from "react";
 const fetcher = (url) => fetch(url).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+  const [isAdmin, setIsAdmin] = useState(true);
+
+  function handleMode() {
+    setIsAdmin(!isAdmin);
+  }
+
+  console.log(isAdmin);
   return (
     <>
       <SWRConfig value={{ fetcher }}>
         <GlobalStyle />
-        <Header />
+        <Header onSetMode={handleMode} />
         <MainContainer>
           <Component {...pageProps} />
         </MainContainer>
