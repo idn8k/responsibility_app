f514fdfed1fa6a5a6feb93c53c8fa0214719c796
Idan <idan.benik@gmail.com>
2025-04-03 14:53:13 +0200

    minor id name update in RegisterForm

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -48,7 +48,7 @@ function RegisterForm() {
           <input
             required
             type="email"
-            id="name"
+            id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
