1113ea813540e96ed56f4da8c9597678e8b9d008
Idan <idan.benik@gmail.com>
2025-04-10 14:42:08 +0200

    fix slug - done

--- components/ui/ChildOperation.jsx
+++ components/ui/ChildOperation.jsx
@@ -1,37 +1,52 @@
 import { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import MenuButton from './MenuButton';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+
 import { FaRegEdit } from 'react-icons/fa';
 import { AiOutlineDelete } from 'react-icons/ai';
 
 const RelativeContainer = styled.div`
   position: relative;
-  transition: transform 0.3s ease-in-out;
+  display: inline-block;
+  height: 24px;
+  width: 24px;
 `;
 
 const StyledDialog = styled.dialog`
   position: absolute;
-  bottom: 500px;
-  left: 185px;
-
+  top: 24px;
+  left: -107px;
   display: flex;
   flex-direction: column;
   gap: 10px;
+  background-color: #fc777e;
+
+  opacity: 0;
+  transform: scale(0.5);
+  pointer-events: none;
 
   border: none;
   border-radius: 20px 0 20px 20px;
 
-  /* &::backdrop {
-    background: rgba(0, 0, 0, 0.2);
+  &::backdrop {
+    background: rgba(0, 0, 0, 0.5);
     backdrop-filter: blur(0.7px);
-  } */
+  }
+
+  ${(props) =>
+    props.$isOpen &&
+    css`
+      opacity: 1;
+      transform: scale(1);
+      pointer-events: auto;
+    `}
 `;
 
 const StyledButton = styled.button`
   border: none;
-  background: none;
+  background-color: transparent;
   height: 50px;
   width: 50px;
   padding: 0;
@@ -42,7 +57,6 @@ const StyledLink = styled(Link)`
   justify-content: center;
   align-items: center;
   border: none;
-  background: none;
   height: 50px;
   width: 50px;
   padding: 0;
@@ -53,50 +67,51 @@ function ChildOperation({ openDeleteModalFunction }) {
   const dialogRef = useRef(null);
   const router = useRouter();
   const { id } = router.query;
-  const [childOpsOpen, setChildOpsOpen] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
-    if (childOpsOpen) {
-      dialogRef.current?.showModal();
+    const handleClickOutside = (event) => {
+      if (dialogRef.current && !dialogRef.current.contains(event.target)) {
+        setMenuOpen(false);
+      }
+    };
+
+    if (menuOpen) {
+      dialogRef.current?.show();
+      document.addEventListener('mousedown', handleClickOutside);
     } else {
       dialogRef.current?.close();
-    }
-  }, [childOpsOpen]);
-
-  function handleClose() {
-    if (!dialogRef.current) {
-      return;
+      document.removeEventListener('mousedown', handleClickOutside);
     }
 
-    dialogRef.current.close();
-    setChildOpsOpen(false);
-    setMenuOpen(false);
-  }
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [menuOpen]);
 
   function toggleChildOps(event) {
-    event.stopPropagation();
     setMenuOpen((menuOpen) => !menuOpen);
-    setChildOpsOpen((childOpsOpen) => !childOpsOpen);
   }
 
   return (
     <RelativeContainer>
       <MenuButton menuOpen={menuOpen} toggleChildOps={toggleChildOps} />
-      <StyledDialog onClick={handleClose} ref={dialogRef}>
-        <StyledLink href={`/child/${id}/editChild`}>
-          <FaRegEdit size="1.8rem" color="ff3566" />
-        </StyledLink>
-        <StyledButton
-          onClick={(event) => {
-            event.stopPropagation();
-            event.preventDefault();
-            openDeleteModalFunction(id);
-          }}
-        >
-          <AiOutlineDelete size="2rem" color="ff3566" />
-        </StyledButton>
-      </StyledDialog>
+      {menuOpen && (
+        <StyledDialog ref={dialogRef} $isOpen={menuOpen}>
+          <StyledLink href={`/child/${id}/editChild`}>
+            <FaRegEdit size="1.8rem" color="#fff" />
+          </StyledLink>
+          <StyledButton
+            onClick={(event) => {
+              event.stopPropagation();
+              event.preventDefault();
+              openDeleteModalFunction(id);
+            }}
+          >
+            <AiOutlineDelete size="2rem" color="#fff" />
+          </StyledButton>
+        </StyledDialog>
+      )}
     </RelativeContainer>
   );
 }
--- components/ui/MenuButton.jsx
+++ components/ui/MenuButton.jsx
@@ -1,13 +1,12 @@
 import styled from 'styled-components';
-import { useState } from 'react';
 
 import { FaTh, FaWindowClose } from 'react-icons/fa';
 
 const StyledButton = styled.button`
   border: none;
-  background: none;
-  height: 50px;
-  width: 50px;
+  background-color: white;
+  height: 24px;
+  width: 24px;
   padding: 0;
 `;
 
@@ -15,9 +14,9 @@ function MenuButton({ menuOpen, toggleChildOps }) {
   return (
     <StyledButton onClick={toggleChildOps}>
       {menuOpen ? (
-        <FaWindowClose size="1.5rem" color="ff3566" />
+        <FaWindowClose size="1.6rem" color="#FC777E" />
       ) : (
-        <FaTh size="1.5rem" color="ff3566" />
+        <FaTh size="1.5rem" color="#FC777E" />
       )}
     </StyledButton>
   );
